d8b220e6263d5359402d0feecd08684b08a2d914
Pedro Guerra <92050904+PedroM-Guerra@users.noreply.github.com>
2025-07-11 12:19:47 -0300

    DTO (Data transfer Objects) implementation

--- rest-with-spring-boot-and-java/src/main/java/pedroMGuerra/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/pedroMGuerra/controllers/PersonController.java
@@ -1,8 +1,8 @@
 package pedroMGuerra.controllers;
 
 
+import pedroMGuerra.data.dto.PersonDTO;
 import pedroMGuerra.services.PersonServices;
-import pedroMGuerra.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +19,14 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
+    public List<PersonDTO> findAll(){
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person findById(@PathVariable("id") Long id){
+    public PersonDTO findById(@PathVariable("id") Long id){
         return service.findById(id);
     }
 
@@ -34,7 +34,7 @@ public class PersonController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person create(@RequestBody Person person){
+    public PersonDTO create(@RequestBody PersonDTO person){
         return service.create(person);
     }
 
@@ -42,7 +42,7 @@ public class PersonController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person update(@RequestBody Person person){
+    public PersonDTO update(@RequestBody PersonDTO person){
         return service.update(person);
     }
 
--- rest-with-spring-boot-and-java/src/main/java/pedroMGuerra/services/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/pedroMGuerra/services/PersonServices.java
@@ -1,6 +1,9 @@
 package pedroMGuerra.services;
 
+import pedroMGuerra.data.dto.PersonDTO;
 import pedroMGuerra.exception.ResourceNotFoundException;
+import static pedroMGuerra.mapper.ObjectMapper.parseListObjects;
+import static pedroMGuerra.mapper.ObjectMapper.parseObject;
 import pedroMGuerra.model.Person;
 import pedroMGuerra.repository.PersonRepository;
 import org.slf4j.Logger;
@@ -23,29 +26,30 @@ public class PersonServices {
     PersonRepository repository;
 
 
-    public List<Person> findAll(){
+    public List<PersonDTO> findAll(){
         logger.info("Finding all People!");
 
-        return repository.findAll();
+        return parseListObjects(repository.findAll(), PersonDTO.class);
     }
 
-    public Person findById(Long id){
+    public PersonDTO findById(Long id){
         logger.info("Finding one Person!");
 
-        Person person = new Person();
-
-        return repository.findById(id)
+        var entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        return parseObject(entity, PersonDTO.class);
     }
 
-    public Person create(Person person){
+    public PersonDTO create(PersonDTO person){
+
         logger.info("Creating one Person!");
+        var entity = parseObject(person, Person.class);
 
-
-        return repository.save(person);
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
-    public Person update(Person person){
+    public PersonDTO update(PersonDTO person){
         logger.info("Updating one Person!");
         Person entity = repository.findById(person.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
@@ -55,7 +59,7 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(person);
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
     public void delete(Long id){
