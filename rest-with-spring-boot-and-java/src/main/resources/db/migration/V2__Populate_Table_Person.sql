47c98fbaa481f3b49fefa8b84a6f6051cadcdc77
Pedro Guerra <92050904+PedroM-Guerra@users.noreply.github.com>
2025-10-09 11:57:03 -0300

    Start to Migrations with Flyway implementation

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Porbandar - India', 'Mahatma', 'Male', 'Ghandi'),
+	(4, 'Aju - Br', 'Pedro', 'Male', 'Guerra'),
+	(6, 'Anchiano - Italia', 'Leonardo', 'Male', 'da Vinci'),
+	(7, 'Kentucky - USA', 'Muhamamd', 'Male', 'Ali'),
+	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(12, 'Aracaju - Brasil', 'Pedro', 'Male', 'Guerra');
